eff991806a337ca70415fcd36eeca676e974d678
Kenny <kenneth.kep@gmail.com>
2022-10-24 21:58:31 -0500

    Last detail in the text

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ function App() {
           imagen='Sarah'
           cargo='Software Engineer'
           empresa='ChatDesk'
-          testimonio='freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.' />
+          testimonio={[<strong>freeCodeCamp was the gateway to my career </strong>, 'as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.']} />
           <Testimonio
           nombre='Emma Bostian'
           pais='Sweden'
           imagen='Emma'
           cargo='Software Engineer'
           empresa='Spotify'
-          testimonio='I ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify' />
+          testimonio={['I ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on', <strong> freeCodeCamp gave me the skills </strong>, ' and confidence I needed to land my dream job as a software engineer at Spotify']} />
       </div>
     </div>
   );
